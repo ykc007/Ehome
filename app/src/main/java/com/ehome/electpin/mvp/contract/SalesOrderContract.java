681550e545cdb9756319de25aca5152338be4a5a
ykc007 <475536348@qq.com>
2020-06-26 20:15:09 +0800

    11

--- app/src/main/java/com/ehome/electpin/mvp/contract/SalesOrderContract.java
+++ app/src/main/java/com/ehome/electpin/mvp/contract/SalesOrderContract.java
@@ -3,8 +3,6 @@ package com.ehome.electpin.mvp.contract;
 import com.fly.tour.api.entity.SalesOrderEntity;
 import com.fly.tour.common.mvp.view.BaseRefreshView;
 
-import java.util.List;
-
 public interface SalesOrderContract {
     interface Presenter {
     }
